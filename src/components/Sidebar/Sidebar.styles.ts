f19c60fb60f8a0aa85001c332593a556695ad744
AbigailSc <abigailsarzuri@gmail.com>
2022-11-29 16:24:15 -0300

    fix: sidebar

--- src/components/Sidebar/Sidebar.styles.ts
+++ src/components/Sidebar/Sidebar.styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+interface ISidebar {
+  isOpen: boolean;
+}
+
 export const Logo = styled.img`
   width: 3em;
   height: 3em;
@@ -9,11 +13,26 @@ export const SidebarContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 220px;
+  width: 230px;
   height: 100%;
   background: rgba(99, 99, 99, 0.1);
   box-shadow: 0 4px 40px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(6.5px);
+  display: ${(props: ISidebar) => (props.isOpen ? 'block' : 'none')};
+`;
+
+export const SidebarContainerHidden = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100px;
+  height: 100%;
+  background: rgba(99, 99, 99, 0.1);
+  box-shadow: 0 4px 40px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(6.5px);
+  display: ${(props: ISidebar) => (props.isOpen ? 'none' : 'flex')};
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const HStack = styled.div`
@@ -65,11 +84,36 @@ export const Button = styled.button`
       transition: 0.2s ease;
       color: #c35df7;
     }
+    & > .subIcon {
+      transition: 0.2s ease;
+      color: #c35df7;
+    }
   }
-  :nth-child(1),
-  :nth-child(2),
-  :nth-child(3) {
+  :nth-child(1) {
     justify-content: flex-start;
+  }
+  & ~ .secondary {
+    justify-content: flex-start;
+  }
+  & > .subIcon {
+    width: 1.5em;
+    height: 1.5em;
+    margin-right: 1em;
+  }
+`;
+
+export const ButtonHidden = styled.button`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  width: 100%;
+  background: transparent;
+  color: #d4d4ea;
+  & > .icon {
+    width: 1.5em;
+    height: 1.5em;
   }
 `;
 
@@ -85,12 +129,4 @@ export const SubItem = styled.div`
   display: flex;
   flex-direction: column;
   /* margin-left: 2.5em; */
-`;
-
-export const Span = styled.div`
-  width: 5px;
-  height: 5px;
-  border-radius: 100%;
-  background-color: aliceblue;
-  margin-right: 2em;
 `;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
 import { AiOutlineHome, AiOutlineCompass, AiOutlineLike, AiOutlineUser } from 'react-icons/ai';
-import { BiPodcast } from "react-icons/bi"
+import { BiPodcast, BiTrendingUp, BiBroadcast, BiMusic } from "react-icons/bi"
 import { MdOutlineMusicVideo, MdExpandMore, MdExpandLess } from "react-icons/md"
-import { Logo, SidebarContainer, Button, ContainerLogo, HStack, SubItem, Span } from './Sidebar.styles';
+import { Logo, SidebarContainer, Button, ContainerLogo, HStack, SubItem, SidebarContainerHidden, ButtonHidden } from './Sidebar.styles';
 
 
 const Sidebar: React.FC = () => {
 
   const [activeItem, setActiveItem] = useState<boolean>(false);
+  const [activeSidebar, setActiveSidebar] = useState<boolean>(false);
 
   const handleActiveItem = (): void => {
     setActiveItem(!activeItem)
@@ -18,72 +19,115 @@ const Sidebar: React.FC = () => {
     setActiveItem(false)
   }
 
+  const handleActiveSidebar = (): void => {
+    setActiveSidebar(true)
+  }
+  const handleDesactiveSidebar = (): void => {
+    setActiveSidebar(false)
+  }
   return (
-    <SidebarContainer>
-      <ContainerLogo>
-        <Logo src="./logo.png" alt="musitracker" />
-      </ContainerLogo>
-      <Button onClick={handleFalseActiveItem}>
-        <div className='effectHover' />
-        <HStack>
-          <AiOutlineHome className="icon" /> <span className='span'>Home</span>
-        </HStack>
-      </Button>
-      <Button onClick={handleActiveItem}>
-        <div className='effectHover' />
-        <HStack>
-          <AiOutlineCompass className="icon" /> Explore
-        </HStack>
-        {activeItem
-          ? <MdExpandLess className="icon" />
-          : <MdExpandMore className="icon" />}
-      </Button>
-      {
-        activeItem && (
-          <SubItem>
-            <Button>
-              <div className='effectHover' />
-              <Span />
-              Trending Music
-            </Button>
-            <Button>
-              <div className='effectHover' />
-              <Span />
-              Top Music
-            </Button>
-            <Button>
-              <div className='effectHover' />
-              <Span />
-              Genres
-            </Button>
-          </SubItem>
-        )
-      }
-      <Button onClick={handleFalseActiveItem}>
-        <div className='effectHover' />
-        <HStack>
-          <BiPodcast className="icon" /> Podcasts
-        </HStack>
-      </Button>
-      <Button onClick={handleFalseActiveItem}>
-        <div className='effectHover' />
-        <HStack>
-          <AiOutlineLike className="icon" /> Favorites
-        </HStack>
-      </Button>
-      <Button onClick={handleFalseActiveItem}>
-        <div className='effectHover' />
-        <HStack>
-          <AiOutlineUser className="icon" /> Artists
-        </HStack>
-      </Button>
-      <Button onClick={handleFalseActiveItem}>
-        <div className='effectHover' />
-        <HStack>
-          <MdOutlineMusicVideo className="icon" /> Albums
-        </HStack>
-      </Button>
-    </SidebarContainer>
+    // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
+    <div onMouseOver={handleActiveSidebar} onMouseOut={handleDesactiveSidebar}>
+      <SidebarContainerHidden isOpen={activeSidebar}>
+        <ContainerLogo>
+          <Logo src="./logo.png" alt="musitracker" />
+        </ContainerLogo>
+        <ButtonHidden>
+          <AiOutlineHome className="icon" />
+        </ButtonHidden>
+        <ButtonHidden>
+          <AiOutlineCompass className="icon" />
+        </ButtonHidden>
+        {activeItem && <>
+          <ButtonHidden>
+            <BiBroadcast className="icon" />
+          </ButtonHidden>
+          <ButtonHidden>
+            <BiTrendingUp className="icon" />
+          </ButtonHidden>
+          <ButtonHidden>
+            <BiMusic className="icon" />
+          </ButtonHidden>
+        </>}
+        <ButtonHidden>
+          <BiPodcast className="icon" />
+        </ButtonHidden>
+        <ButtonHidden>
+          <AiOutlineLike className="icon" />
+        </ButtonHidden>
+        <ButtonHidden>
+          <AiOutlineUser className="icon" />
+        </ButtonHidden>
+        <ButtonHidden>
+          <MdOutlineMusicVideo className="icon" />
+        </ButtonHidden>
+      </SidebarContainerHidden>
+      <SidebarContainer isOpen={activeSidebar}>
+        <ContainerLogo>
+          <Logo src="./logo.png" alt="musitracker" />
+        </ContainerLogo>
+        <Button onClick={handleFalseActiveItem}>
+          <div className='effectHover' />
+          <HStack>
+            <AiOutlineHome className="icon" /> <span className='span'>Home</span>
+          </HStack>
+        </Button>
+        <Button onClick={handleActiveItem}>
+          <div className='effectHover' />
+          <HStack>
+            <AiOutlineCompass className="icon" /> Explore
+          </HStack>
+          {activeItem
+            ? <MdExpandLess className="icon" />
+            : <MdExpandMore className="icon" />}
+        </Button>
+        {
+          activeItem && (
+            <SubItem>
+              <Button className='secondary'>
+                <div className='effectHover' />
+                <BiBroadcast className='subIcon' />
+                Trending Music
+              </Button>
+              <Button className='secondary'>
+                <div className='effectHover' />
+                <BiTrendingUp className='subIcon' />
+                Top Music
+              </Button>
+              <Button className='secondary'>
+                <div className='effectHover' />
+                <BiMusic className='subIcon' />
+                Genres
+              </Button>
+            </SubItem>
+          )
+        }
+        <Button onClick={handleFalseActiveItem}>
+          <div className='effectHover' />
+          <HStack>
+            <BiPodcast className="icon" /> Podcasts
+          </HStack>
+        </Button>
+        <Button onClick={handleFalseActiveItem}>
+          <div className='effectHover' />
+          <HStack>
+            <AiOutlineLike className="icon" /> Favorites
+          </HStack>
+        </Button>
+        <Button onClick={handleFalseActiveItem}>
+          <div className='effectHover' />
+          <HStack>
+            <AiOutlineUser className="icon" /> Artists
+          </HStack>
+        </Button>
+        <Button onClick={handleFalseActiveItem}>
+          <div className='effectHover' />
+          <HStack>
+            <MdOutlineMusicVideo className="icon" /> Albums
+          </HStack>
+        </Button>
+      </SidebarContainer>
+    </div>
   );
 };
 
