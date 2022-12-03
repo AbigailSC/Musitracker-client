95c54ac9904b2270f97898f37f9f1093e88b16d3
AbigailSc <abigailsarzuri@gmail.com>
2022-12-03 00:45:39 -0300

    fix: searchbar without event key

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState, KeyboardEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { SearchbarContainer, SearchbarInput, Icon } from './Searchbar.styles';
 import { BsSearch } from 'react-icons/bs';
 import { useCustomDispatch } from '../../hooks/redux';
@@ -20,19 +20,19 @@ const Searchbar: React.FC = () => {
     }
   };
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLElement>): void => {
-    if (e.key === 'Enter') {
-      handleSearch();
-    }
-    console.log('enter');
-  };
+  // const handleKeyDown = (e: KeyboardEvent<HTMLElement>): void => {
+  //   if (e.key === 'Enter') {
+  //     handleSearch();
+  //   }
+  // };
+
+  // onKeyDown={handleKeyDown} in searchbar input
   return (
     <SearchbarContainer>
       <SearchbarInput
         type="text"
         placeholder="Search by artists, songs and albums"
         onChange={(e) => handleInputChange(e)}
-        onKeyDown={handleKeyDown}
       />
       <Link to={name === '' ? '' : `/search=${name}`}>
         <Icon onClick={handleSearch}>
