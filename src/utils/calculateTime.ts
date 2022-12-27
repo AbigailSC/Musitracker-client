89b2fd1147313a9fafa8de28a1b8fa3329ca8be2
AbigailSc <abigailsarzuri@gmail.com>
2022-12-26 21:03:21 -0300

    fix: claculateTime util

--- src/utils/calculateTime.ts
+++ src/utils/calculateTime.ts
@@ -1,6 +1,6 @@
 const calculateTime = (secs: number): string => {
   const minutes = Math.floor(secs / 60);
-  const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  const returnedMinutes = minutes < 10 ? `${minutes}` : `${minutes}`;
   const seconds = Math.floor(secs % 60);
   const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
   return `${returnedMinutes}:${returnedSeconds}`;
